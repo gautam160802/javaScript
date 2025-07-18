7ba591370d9257ac3df5acd7af87ca16c5f86024
gautam kumar <120775495+gautam160802@users.noreply.github.com>
2025-07-18 19:03:17 +0530

    adding datatypes

---
+++ chpt1/datatypes.js
@@ -0,0 +1,23 @@
+console.log("apna college");
+
+fullname = "gautam";
+age = 55;
+price = "99";
+radius = "14";
+a = "null";
+
+console.log(fullname, age, price, radius, a);
+
+var age = "44";
+var age = "88";
+var age = "55";
+
+console.log("your age is :", age);
+
+const student = {
+  fullname: "Gautam",
+  age: 20,
+};
+
+student["age"] = student["age"] + 1;
+console.log(student["age"]);
